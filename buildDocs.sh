c339a3e902a7d943415ce7c06778c83dd1c1b713
Kerri Shotts <kerrishotts@gmail.com>
2013-04-19 21:44:34 -0500

    switch to yuidoc

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 #
-# Build all javascript docs via jsdoc3
+# Build all javascript docs via yuidoc 
 #
-export HOME_DIR=`pwd`
-export FRAMEWORK_HOME=$HOME_DIR/framework
-export DOCS_DIR=$HOME_DIR/docs
-export JSDOC_HOME=/Applications/jsdoc
-$JSDOC_HOME/jsdoc --verbose $FRAMEWORK_HOME -r -c $FRAMEWORK_HOME/docsconf.json -d $DOCS_DIR $*
+yuidoc .
 
